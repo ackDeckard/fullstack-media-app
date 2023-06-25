989117c7bc217039e38182d56e795acea77a8b71
Pablo <ackdeckard@proton.me>
2023-06-25 20:27:06 -0300

    Fix: desktop layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,10 +1,9 @@
-import GetData from "../components/GetData";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
 
 const DashBoardLayout = ({ children }) => {
   return (
-    <div>
+    <div className="bg-backgroundColor lg:grid lg:grid-cols-[160px_1fr] lg:grid-rows-[min-content_1fr] ">
       <Header />
       <SearchBar />
       {children}
