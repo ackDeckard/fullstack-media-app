b2ef19d410275d1e7424c53d5357b0ae56408e9d
Pablo <ackdeckard@proton.me>
2023-06-25 20:25:48 -0300

    Add: tv-series filter

--- app/components/GetData.tsx
+++ app/components/GetData.tsx
@@ -4,28 +4,170 @@ import { database } from "../firebase/firebase";
 import { collection, getDocs } from "firebase/firestore";
 import TrendingItems from "./TrendingItems";
 import FilterMovies from "./FilterMovies";
+import ShowAllMedias from "./ShowAllMedias";
+import FilterTvSeries from "./FilterTvSeries";
 
-const GetData = ({ showTrendingItems, showMoviesByCategory }) => {
-  const [retrievedData, setRetrievedData] = useState([]);
+type PropsType = {
+  showTrendingItems: boolean;
+  showMoviesByCategory: boolean;
+  showTvSeries: boolean;
+  showAll: boolean;
+};
 
-  useEffect(() => {
-    const getMovieData = async () => {
-      try {
-        const timeTrackingCollectionRef = collection(database, "items");
-        const data = await getDocs(timeTrackingCollectionRef);
-        const filteredData = data.docs.map((doc) => doc.data());
-        setRetrievedData(filteredData);
-      } catch (error) {
-        console.error(error);
-      }
-    };
-    getMovieData();
-  }, []);
+const Data = [
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+  {
+    title: "Beyond Earth",
+    image: "steam.jpg",
+    year: 2019,
+    category: "Movie",
+    rating: "PG",
+    isBookmarked: false,
+    isTrending: true,
+  },
+];
+
+const GetData = ({
+  showTrendingItems,
+  showMoviesByCategory,
+  showTvSeries,
+  showAll,
+}: PropsType) => {
+  const [retrievedData, setRetrievedData] = useState(Data);
+  const [isDataFetched, setIsDataFetched] = useState(false);
+
+  console.log("got data", retrievedData);
+
+  // useEffect(() => {
+  //   if (!isDataFetched) {
+  //     const getMovieData = async () => {
+  //       try {
+  //         const timeTrackingCollectionRef = collection(database, "items");
+  //         const data = await getDocs(timeTrackingCollectionRef);
+  //         const filteredData = data.docs.map((doc) => doc.data());
+  //         if (filteredData) setRetrievedData(filteredData);
+  //         setIsDataFetched(true);
+  //       } catch (error) {
+  //         console.error(error);
+  //       }
+  //     };
+  //     getMovieData();
+  //   }
+  // }, [isDataFetched]);
 
   return (
     <div className="h-full">
       {showTrendingItems && <TrendingItems data={retrievedData} />}
       {showMoviesByCategory && <FilterMovies data={retrievedData} />}
+      {showTvSeries && <FilterTvSeries data={retrievedData} />}
+      {showAll && <ShowAllMedias data={retrievedData} />}
     </div>
   );
 };
