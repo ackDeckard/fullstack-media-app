6931b7db770b81a61c740e1e5bf4183f76b3affc
Pablo <ackdeckard@proton.me>
2023-06-26 21:35:00 -0300

    Add: loading state

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,13 +1,17 @@
+import { Suspense } from "react";
 import Header from "../components/Header";
 import SearchBar from "../components/SearchBar";
+import Loading from "./loading";
 
 const DashBoardLayout = ({ children }) => {
   return (
-    <div className="bg-backgroundColor lg:grid lg:grid-cols-[160px_1fr] lg:grid-rows-[min-content_1fr] ">
-      <Header />
-      <SearchBar />
-      {children}
-    </div>
+    <Suspense fallback={<Loading />}>
+      <div className="bg-backgroundColor lg:grid lg:grid-cols-[160px_1fr] lg:grid-rows-[min-content_1fr] ">
+        <Header />
+        <SearchBar />
+        {children}
+      </div>
+    </Suspense>
   );
 };
 
